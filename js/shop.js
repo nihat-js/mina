f66751fe46c2cb14508479ae40aa587c0bfa5548
nihat.js <abdullazadenihat@gmail.com>
2022-12-15 11:16:37 +0400

    shop section added

--- js/shop.js
+++ js/shop.js
@@ -5,16 +5,21 @@ let box1El = document.querySelector('.product .product-content .box1')
 
 fetch("api/products.json")
     .then(res => res.json())
-    .then (json => {
+    .then(json => {
         Products = json.products
         loadProduct()
     })
 
 function loadProduct() {
-    Products.forEach((p)=>{
+    Products.forEach((p) => {
         box1El.innerHTML += `
-        <img src='${p.img}
-        
+        <div class='el'>
+            <div class='img-wrap'>
+                <img src='${p.src}'/> 
+            </div>
+            <p class='title'>  ${p.title}   </p>
+            <h2 class='price'>  ${p.price}   </h2>
+        </div>
         `
     })
 }
