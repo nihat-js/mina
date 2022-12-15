dd8d8f3f9fd875b03a0db912a0762c7ac6600834
Nihat <abdullazadenihat@gmail.com>
2022-12-15 19:51:51 +0400

    card style added

--- js/product.js
+++ js/product.js
@@ -21,7 +21,6 @@ function main() {
    console.log(Products)
    let Product = Products.find(x => x.id == product_id)
    let foo = Cart.find(x => x.product_id == product_id)
-
    document.querySelector('.product-full .content').innerHTML = `
             <div class="img-wrap">
 					<img src="${Product.src}" alt="">
@@ -30,9 +29,13 @@ function main() {
 					<h1 class="title"> ${Product.title}  </h1>
 					<a class='back' href="search.html"> Back to catalog </a>
                <p class='price'   > $${Product.price}  </p>    
-					<p class="count"> ${foo.count > 0 ? foo.count + ' items in the bag' : ''}  </p>
+					<p class="count"> ${foo?.count > 0 ? foo?.count + ' items in the bag' : ''}  </p>
+               <div class='add-wrap'>
 					<button class='add' onclick='add_to_cart()'> Add More </button>
-               <a href='checkout'> ${foo.count > 0} Checkout   </a>
+               </div>
+               <div class='checkout-wrap'>
+                  <a class='checkout' href='checkout.html'>  ${foo?.count > 0 ? 'Checkout' : ''}     </a>
+               </div>
 					<div class="details-group">
 						<p class="details-headline"> Product Details</p>
 						<p class="details-text">  ${Product.details} </p>
@@ -54,7 +57,7 @@ function main() {
 
 
 function add_to_cart() {
-   let product = Cart.find(x => x.id == product_id)
+   let product = Cart.find(x => x.product_id == product_id)
    if (product) {
       product.count++
    } else {
@@ -63,6 +66,7 @@ function add_to_cart() {
          count: 1,
       })
    }
+   main()
    updateLocalStorage()
 }
 
